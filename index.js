f31fb77ffe8c78de0ba197afa45216e74f22c872
TheyKay <murraykyleb@gmail.com>
2020-08-23 01:32:00 -0600

    pseudocode in js

---
+++ index.js
@@ -0,0 +1,5 @@
+// run current weather
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+// to get coordinates of city matching search term from text box
+// then use those coordinates to run one call
+// https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={YOUR API KEY}
