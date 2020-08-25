c721349a74b0e45ae42136cfc380261c5a6dbebd
TheyKay <murraykyleb@gmail.com>
2020-08-24 23:26:33 -0600

    added fontawesome so it works now and started working on layout

--- index.js
+++ index.js
@@ -4,16 +4,58 @@
 // then use those coordinates to run one call
 // https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={YOUR API KEY}
 
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-// WHEN I open the weather dashboard
-// THEN I am presented with the last searched city forecast
+// 1 search for city --> see current/future conditions, add city to search history
+// 2 current weather shows
+    // city name
+    // Date
+    // icon representation of weather conditions
+    // temperature
+    // humidity
+    // wind speed
+    // UV index
+// 3      show color that indicates favorable, moderate, severe
+// 4 future weather conditions
+    // ecah day on 5-day forecast shows:
+        // date
+        // icon representation of weather conditions
+        // temperature (max and min)
+        // humidity
+// 5 click on city in search history, show all same stuff
+// 6 opening page shows last searched city info
+
+// assign  classes in quotes to call icon
+const icons = {
+    tempHi: "fas fa-temperature-high",
+    tempLo: "fas fa-temperature-low",
+    smog: "fas fa-smog",
+    bolt: "fas fa-bolt",
+    cloud: "fas fa-cloud",
+    cloudRain: "fas fa-cloud-rain",
+    cloudShower: "fas fa-cloud-showers-heavy",
+    cloudSun: "fas fa-cloud-sun",
+    cloudSunRain: "fas fa-cloud-sun-rain",
+    snow: "fas fa-snowflake",
+    sun: "fas fa-sun",
+    wind: "fas fa-wind"
+}
+
+// get this from text entry field, populate drop-down menu to select correct entry
+let citySearch = $('#autocomplete-input').val();
+console.log(citySearch);
+
+// to search for cities, get lat/long
+// https://rapidapi.com/dev132/api/city-geo-location-lookup/endpoints
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=" + citySearch,
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "devru-latitude-longitude-find-v1.p.rapidapi.com",
+		"x-rapidapi-key": "5573b1307dmshac7c49573ae8266p15de2ajsnf70e1968529e"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
